100d5e79e58a7d2f9fec88c335930f33256259ef
SUBASREE K <124561940+34suba2023@users.noreply.github.com>
2023-07-26 07:16:38 +0530

    Add files via upload

---
+++ Method 2and 3 Using Nested if-else Statements  to check whether a given number is Positive or Negative.py
@@ -0,0 +1,16 @@
+num=-20
+if num>=0:
+    if num==0:
+       print('Zero')
+    else:
+        print('Positive')
+else:
+    print("Negative")
+    
+
+
+#method 3 ternary operator to check whether a number is Positive or Negative
+
+
+a=50
+print("Positive" if a>=0 else "Negative")
